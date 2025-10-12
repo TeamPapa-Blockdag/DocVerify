61573a6f606e1c85d526e2945d89b810f36986d5
DevTimmy <oyinloyematthew6@gmail.com>
2025-10-12 01:13:51 +0100

    Fixing the typescript error

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,13 +1,13 @@
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 
 
-export function getRpcProvider() {
-    return new ethers.providers.JsonRpcProvider(import.meta.env.VITE_BLOCK_DAG_RPC)
-}
+// export function getRpcProvider() {
+//     return new ethers.providers.JsonRpcProvider(import.meta.env.VITE_BLOCK_DAG_RPC)
+// }
 
-export function getInjectedProvider() {
-    const provider = (window as any).ethereum;
-    if (!provider) throw new Error("MetaMask not installed");
-    return new ethers.providers.Web3Provider(provider);
-}
+// export function getInjectedProvider() {
+//     const provider = (window as any).ethereum;
+//     if (!provider) throw new Error("MetaMask not installed");
+//     return new ethers.providers.Web3Provider(provider);
+// }
 
